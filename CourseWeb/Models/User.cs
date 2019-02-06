b49b43ee0b0ba27c9a19e7bd9f7cf765336b0ada
kirsh <kirsh@bluelotus360.com>
2019-02-06 14:01:30 +0530

    First commit

---
+++ CourseWeb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseWeb.Models
+{
+    public class User
+    {
+    }
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    } 
+}
---
+++ CourseWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseWeb.Startup))]
+namespace CourseWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
