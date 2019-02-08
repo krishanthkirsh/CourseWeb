38bb9e2fb861f7c0ebb4a3f2701edb2f7a072401
kirsh <kirsh@bluelotus360.com>
2019-02-08 11:33:00 +0530

    hash Function

--- CourseWeb/Controllers/LoginController.cs
+++ CourseWeb/Controllers/LoginController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +16,8 @@ namespace CourseWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                string hashPassword = getHashPassword(model.PassWord);
+                Console.WriteLine(hashPassword);
             }
             else
             {
@@ -27,5 +30,25 @@ namespace CourseWeb.Controllers
         {
             return View();
         }
+
+
+        private static string getHashPassword(string textPassword)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(textPassword));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
     }
 }
